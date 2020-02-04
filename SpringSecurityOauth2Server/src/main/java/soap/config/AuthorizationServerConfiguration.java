980dbfe9636304c010be9d1a9597abd370fd85ba
ZhangPY <overun9299@163,com>
2020-02-05 00:46:10 +0800

    将access_token等数据放入redis

--- SpringSecurityOauth2Server/src/main/java/soap/config/AuthorizationServerConfiguration.java
+++ SpringSecurityOauth2Server/src/main/java/soap/config/AuthorizationServerConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -12,6 +13,8 @@ import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+
 import javax.sql.DataSource;
 
 
@@ -22,10 +25,19 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
     @Autowired
     private DataSource dataSource;
 
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
+
+    /**
+     * token保存在内存中（也可以保存在数据库、Redis中）
+     * @return
+     */
     @Bean
     public TokenStore tokenStore() {
-        /** 基于 JDBC 实现，令牌保存到数据 */
-        return new JdbcTokenStore(dataSource);
+//        /** 基于 JDBC 实现，令牌保存到数据 */
+//        return new JdbcTokenStore(dataSource);
+        /** token保存在内存中（也可以保存在数据库、Redis中） */
+        return new RedisTokenStore(redisConnectionFactory);
     }
 
     @Bean
--- SpringSecurityOauth2Server/src/main/java/soap/controller/LoginPageController.java
+++ SpringSecurityOauth2Server/src/main/java/soap/controller/LoginPageController.java
@@ -3,6 +3,14 @@ package soap.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import soap.utils.CookieUtil;
+import soap.utils.SessionUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 /**
  * Created by ZhangPY on 2020/2/3
@@ -25,5 +33,32 @@ public class LoginPageController {
         model.addAttribute("loginProcessUrl", "/auth2/auth/authorize");
 
         return "base-login";
+    }
+
+
+    /**
+     * 登出
+     */
+    @GetMapping("/auth/loginOut")
+    @ResponseBody
+    public void loginOut() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        SessionUtils.removeAttr(request , "SPRING_SECURITY_CONTEXT");
+        SessionUtils.removeAttr(request , "org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository.CSRF_TOKEN");
+    }
+
+
+    /**
+     * 取出cookie中的身份令牌
+     * @return
+     */
+    private String getTokenFormCookie(){
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        Map<String, String> map = CookieUtil.readCookie(request, "JSESSIONID");
+        if(map!=null && map.get("JSESSIONID")!=null){
+            String sessionId = map.get("JSESSIONID");
+            return sessionId;
+        }
+        return null;
     }
 }
