51e521b13cc173ea1eccc88d2450fae9acb43c84
ZhangPY <overun9299@163,com>
2020-02-05 00:47:39 +0800

    添加退出功能(demo)

--- SpringSecurityOauth2Resources/src/main/java/soap/controller/TokenController.java
+++ SpringSecurityOauth2Resources/src/main/java/soap/controller/TokenController.java
@@ -1,6 +1,8 @@
 package soap.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import soap.service.TokenService;
@@ -11,12 +13,23 @@ import soap.service.TokenService;
  * overun9299@163.com
  * Explain:
  */
-@RestController
+@Controller
 public class TokenController {
 
 
     @Autowired
     private TokenService tokenService;
+
+//    /**
+//     * 通过code获取AccessToken
+//     * @param code
+//     * @return
+//     */
+//    @GetMapping(value = "/getAccessTokenByCode")
+//    public String getAccessTokenByCode(String code) {
+//
+//        return tokenService.getAccessTokenByCode(code);
+//    }
 
     /**
      * 通过code获取AccessToken
@@ -24,8 +37,9 @@ public class TokenController {
      * @return
      */
     @GetMapping(value = "/getAccessTokenByCode")
-    public String getAccessTokenByCode(String code) {
-
-        return tokenService.getAccessTokenByCode(code);
+    public String getAccessTokenByCode(String code , Model model) {
+        String accessTokenByCode = tokenService.getAccessTokenByCode(code);
+        model.addAttribute("data" , accessTokenByCode);
+        return "base-grant";
     }
 }
--- SpringSecurityOauth2Resources/src/main/java/soap/service/impl/AcPermissionServiceImpl.java
+++ SpringSecurityOauth2Resources/src/main/java/soap/service/impl/AcPermissionServiceImpl.java
@@ -2,10 +2,19 @@ package soap.service.impl;
 
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2RefreshToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.store.redis.JdkSerializationStrategy;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStoreSerializationStrategy;
 import org.springframework.stereotype.Service;
 import soap.domain.AcPermission;
 import soap.mapper.AcPermissionMapper;
+import soap.redis.JedisTools;
 import soap.service.AcPermissionService;
+import soap.utils.SerializeUtil;
 
 import java.util.List;
 
@@ -23,8 +32,23 @@ public class AcPermissionServiceImpl implements AcPermissionService {
     @Autowired
     private AcPermissionMapper acPermissionMapper;
 
+    @Autowired
+    private JedisTools jedisTools;
+
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
+
     @Override
     public String getAcPermission() {
+        RedisTokenStore redisTokenStore = new RedisTokenStore(redisConnectionFactory);
+        OAuth2RefreshToken oAuth2RefreshToken = redisTokenStore.readRefreshToken("d2e17f0d-7ac9-485f-89d5-b2e1794f3586");
+        OAuth2Authentication oAuth2Authentication = redisTokenStore.readAuthentication("bc1757e8-8d07-4bcd-92c6-9fbf413d1fa7");
+        OAuth2AccessToken oAuth2AccessToken = redisTokenStore.readAccessToken("bc1757e8-8d07-4bcd-92c6-9fbf413d1fa7");
+
+
+
+
+
         List<AcPermission> acPermissions = acPermissionMapper.selectByExample(null);
         return JSONObject.toJSONString(acPermissions);
     }
